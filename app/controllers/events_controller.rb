ca15f881dba3319af4d0c70bac89c7c74695a11c
Peter Wang <happypeter1983@gmail.com>
2012-12-11 11:57:13 +0800

    new event can be created now

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,4 +4,12 @@ class EventsController < ApplicationController
   def new
     @event = Event.new
   end
+  def create
+    event = Event.new(params[:event])
+    if event.save
+      redirect_to :root, :notice => "New event created successfully!"
+    else
+      redirect_to :root, :notice => "Failed to creat new event!"
+    end
+  end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,3 @@
 class Event < ActiveRecord::Base
+  attr_accessible :title, :content
 end
