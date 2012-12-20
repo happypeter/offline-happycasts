f6b697b2762b8772bf52d3078b26a867e1749296
Peter Wang <happypeter1983@gmail.com>
2012-12-20 11:07:08 +0800

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121211073910) do
+ActiveRecord::Schema.define(:version => 20121220030551) do
 
   create_table "events", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(:version => 20121211073910) do
     t.string   "title"
     t.text     "content"
     t.datetime "time"
+  end
+
+  create_table "users", :force => true do |t|
+    t.boolean  "admin"
+    t.boolean  "in_beijing"
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
