41168e6d58b759a1705a17dc9fdd3bed12e7d585
Peter Wang <happypeter1983@gmail.com>
2012-12-20 16:09:39 +0800

    user can edit his own profile now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,5 +41,17 @@ class UsersController < ApplicationController
     redirect_to root_url, :notice => "You have been logged out."
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      redirect_to user_path(@user)
+    else
+      render 'edit'
+    end
+  end
 
 end
