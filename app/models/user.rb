57c95bd3743f413f26efb7f230a127280033d031
Peter Wang <happypeter1983@gmail.com>
2012-12-20 12:18:31 +0800

    we need a token for login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :in_beijing, :name
+  before_create { generate_token(:token) }
+
+  def generate_token(column)
+    begin
+      self[column] = SecureRandom.urlsafe_base64
+    end while User.exists?(column => self[column])
+  end
 end
