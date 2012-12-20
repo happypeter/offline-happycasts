7fc239e7460f85fc63d1b50024773275516a1b56
Peter Wang <happypeter1983@gmail.com>
2012-12-20 17:32:45 +0800

    add about page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Offline::Application.routes.draw do
   resources :users
   match "user_login" => "users#login"
   match "login" => "users#login_form"
+  match "about" => "info#about"
   match "logout" => "users#logout", :as => "logout"
   root :to => 'events#index'
   match "signup" => "users#signup", :as => "signup"
