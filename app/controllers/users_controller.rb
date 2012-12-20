aeb45fdde5df06af117940a539e8cb395a0e58a8
Peter Wang <happypeter1983@gmail.com>
2012-12-20 11:57:54 +0800

    create action for the form submit
    for security reason, don't allow `admin` to be changed from client side

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,12 @@ class UsersController < ApplicationController
   def signup
     @user = User.new
   end
+  def create
+    user = User.new(params[:user])
+    if user.save
+      redirect_to :root
+    else
+      render "signup"
+    end
+  end
 end
