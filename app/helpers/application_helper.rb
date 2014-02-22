ab8cd6111e290e5ff6c56b3ed3b2bb2fa8bcdaef
YubTan <seventanyb@gmail.com>
2014-02-22 14:37:28 +0800

    add markdown support

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+
+  class HTMLwithPygments < Redcarpet::Render::HTML
+    def block_code(code, language)
+      languages = ["bash","ruby","python","c"]
+      if !languages.include?language
+        language = nil
+      end
+      sha = Digest::SHA1.hexdigest(code)
+      Rails.cache.fetch ["code", language, sha].join('-') do
+        Pygments.highlight(code, lexer: language)
+      end
+    end
+  end
+
+  def markdown(text)
+    renderer = HTMLwithPygments.new(hard_wrap: true, filter_html: true)
+    options = {
+      autolink: true,
+      no_intra_emphasis: true,
+      fenced_code_blocks: true,
+      lax_html_blocks: true,
+      strikethrough: true,
+      superscript: true
+    }
+    Redcarpet::Markdown.new(renderer, options).render(text).html_safe
+  end
 end
